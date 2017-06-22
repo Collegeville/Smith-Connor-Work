295e743645929b358853cd20f97a06eb80894c2b
Connor Smith <connorsmith87292gmail.com>
2017-06-22 11:02:33 -0500

    Allow user-input in sin_net.py

--- Python_Examples/sin_net.py
+++ Python_Examples/sin_net.py
@@ -15,10 +15,9 @@ DATA_FILE = 'sin_data.csv'
 
 #Number of neurons in each layer
 neurons_layer1 = 50
-neurons_layer2 = 60
-neurons_layer3 = 50
+neurons_layer2 = 50
+neurons_layer3 = 75
 
-#batch_size =  (not large enough data set)
 
 x = tf.placeholder('float')
 y = tf.placeholder('float')
@@ -42,9 +41,9 @@ def get_data(filename):
 		sess.run(tf.global_variables_initializer())
 		coord = tf.train.Coordinator()
 		threads = tf.train.start_queue_runners(coord=coord)
-		rad_list = list()
+		deg_list = list()
 		sin_list = list()
-		test_rad_list = list()
+		test_deg_list = list()
 		test_val_list = list()
 
 		data_size = 721
@@ -52,23 +51,23 @@ def get_data(filename):
 		for i in range(data_size):
 			example, label = sess.run([features, col2])
 			if i <= 500:
-				rad_list.append(example)
+				deg_list.append(example)
 				sin_list.append(label)
 			else:
-				test_rad_list.append(example)
+				test_deg_list.append(example)
 				test_val_list.append(label)
 
 		#Normalize data between 0 and 1(temporary fix for debugging)
-		rad = [(r-min(rad_list))/(max(rad_list)-min(rad_list)) for r in rad_list]
-		sin = [(s-min(sin_list))/(max(sin_list)-min(sin_list)) for s in sin_list]
-		test_rad = [(tr-min(test_rad_list))/(max(test_rad_list)-min(test_rad_list)) for tr in test_rad_list]
-		test_sin = [(ts-min(test_val_list))/(max(test_val_list)-min(test_val_list)) for ts in test_val_list]
+		#deg = [(r-min(rad_list))/(max(rad_list)-min(rad_list)) for r in rad_list]
+		#sin = [(s-min(sin_list))/(max(sin_list)-min(sin_list)) for s in sin_list]
+		#test_deg = [(tr-min(test_rad_list))/(max(test_rad_list)-min(test_rad_list)) for tr in test_rad_list]
+		#test_sin = [(ts-min(test_val_list))/(max(test_val_list)-min(test_val_list)) for ts in test_val_list]
 
 
 	coord.request_stop()
 	coord.join(threads)
 
-	return rad, sin, test_rad, test_sin
+	return deg_list, sin_list, test_deg_list, test_val_list
 
 
 
@@ -80,53 +79,74 @@ def model(input_data):
 						'biases': tf.Variable(tf.zeros(neurons_layer2))}
 	hidden3 = {'weights': tf.Variable(tf.random_normal([neurons_layer2, neurons_layer3])),
 						'biases': tf.Variable(tf.zeros(neurons_layer3))}
-	output = {'weights': tf.Variable(tf.random_normal([neurons_layer3, 1])),
+	output = {'weights': tf.Variable(tf.random_normal([neurons_layer2, 1])),
 						'biases': tf.Variable(tf.zeros(1))}
 
 
 	layer1 = tf.add(tf.multiply(input_data, hidden1['weights']), hidden1['biases'])
-	layer1 = tf.nn.relu(layer1)
+	layer1 = tf.nn.tanh(layer1)
 
 	layer2 = tf.add(tf.matmul(layer1, hidden2['weights']), hidden2['biases'])
-	layer2 = tf.nn.relu(layer2)
+	layer2 = tf.nn.tanh(layer2)
 
-	layer3 = tf.add(tf.matmul(layer2, hidden3['weights']), hidden3['biases'])
-	layer3 = tf.nn.relu(layer3)
+	#layer3 = tf.add(tf.matmul(layer2, hidden3['weights']), hidden3['biases'])
+	#layer3 = tf.nn.relu(layer3)
+	#layer3 = tf.nn.dropout(layer3, .5)
 
-	output = tf.add(tf.matmul(layer3, output['weights']), output['biases'])
+	output = tf.add(tf.matmul(layer2, output['weights']), output['biases'])
 
 	return output
 
 #Train the neural network model
 def train_net(x,y):
 	pred = model(x)
-	cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=pred,labels=y))
+	cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y,logits=pred))
 
 	#Optimize weights and biases in order to minimize cost function
-	optimizer = tf.train.AdamOptimizer(learning_rate=.1).minimize(cost)
+	optimizer = tf.train.AdamOptimizer(.01).minimize(cost)
 
-	epochs = 2
+	epochs = 3
 
-	#Run computation graph that feeds 
+	saver = tf.train.Saver()
+
+	#Run computation graph that feeds pre-processed data through network
+	#and the cost and optimizer functions
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 
 		train_x, train_y, test_x, test_y = get_data(DATA_FILE)
 
+
 		for epoch in range(epochs):
 			epoch_loss = 0
-			step = 0
-			for _ in range((len(train_x))):
-				_, c = sess.run([optimizer, cost], feed_dict = {x: train_x, y: train_y})
+
+			batch_size = 5
+			i = 0
+			while i < (len(train_x)):
+				start = i 
+				end = i + batch_size
+
+				batch_x = np.array(train_x[start:end])
+				batch_y = np.array(train_y[start:end])
+
+				_, c = sess.run([optimizer, cost], feed_dict = {x: batch_x, y: batch_y})
 				epoch_loss += c
 
-				step += 1
+				i += batch_size
 
 			print('Epoch: ', epoch, ' loss: ', epoch_loss)
 
-		correct = tf.equal(tf.argmax(pred), tf.argmax(y))
+		saver.save(sess,'.\model')
+
+		correct = tf.equal(tf.round(pred), tf.round(y))
 		accuracy = tf.reduce_mean(tf.cast(correct,'float'))
 		print('Accuracy:', accuracy.eval({x: test_x, y: test_y}))
+
+		input_for_pred = input("Enter degree value to be approximated \n")
+		input_for_pred = float(input_for_pred)
+		feed_dict = {x: input_for_pred}
+		approx = pred.eval(feed_dict)
+		print(approx)
 
 train_net(x,y)
 				
