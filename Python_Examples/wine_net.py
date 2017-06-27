2298c2f7545401f3f10c48d9a4e90e9002be257d
Connor Smith <connorsmith87292gmail.com>
2017-06-27 14:24:21 -0500

    Simplify to increase accuracy

--- Python_Examples/wine_net.py
+++ Python_Examples/wine_net.py
@@ -3,7 +3,7 @@ import numpy as np
 
 DATA_FILE = 'wine.csv'
 
-neurons_layer1 = 10
+neurons_layer1 = 9
 neurons_layer2 = 15
 neurons_layer3 = 15
 
@@ -54,19 +54,19 @@ def model(input_data):
 						'biases': tf.Variable(tf.zeros(neurons_layer2))}
 	hidden3 = {'weights': tf.Variable(tf.random_normal([neurons_layer2, neurons_layer3])),
 						'biases': tf.Variable(tf.zeros(neurons_layer3))}
-	output = {'weights': tf.Variable(tf.random_normal([neurons_layer3, 4])),
+	output = {'weights': tf.Variable(tf.random_normal([neurons_layer2, 4])),
 						'biases': tf.Variable(tf.zeros(4))}
 
 	layer1 = tf.add(tf.matmul(input_data, hidden1['weights']), hidden1['biases'])
-	layer1 = tf.nn.dropout(tf.nn.relu(layer1), .5)
+	#layer1 = tf.nn.dropout(tf.nn.relu(layer1), .5)
 
 	layer2 = tf.add(tf.matmul(layer1, hidden2['weights']), hidden2['biases'])
-	layer2 = tf.nn.dropout(tf.nn.relu(layer2), .5)
+	#layer2 = tf.nn.dropout(tf.nn.relu(layer2), .5)
 
-	layer3 = tf.add(tf.matmul(layer2, hidden3['weights']), hidden3['biases'])
-	layer3 = tf.nn.dropout(tf.nn.relu(layer3), .5)
+	#layer3 = tf.add(tf.matmul(layer2, hidden3['weights']), hidden3['biases'])
+	#layer3 = tf.nn.dropout(tf.nn.relu(layer3), .5)
 
-	output = tf.add(tf.matmul(layer3, output['weights']), output['biases'])
+	output = tf.add(tf.matmul(layer2, output['weights']), output['biases'])
 
 	return output
 
@@ -77,7 +77,7 @@ def train_net(x,y):
 
 	cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.reshape(y, [batch_size]), logits=pred))
 
-	optimizer = tf.train.AdagradOptimizer(.01).minimize(cost)
+	optimizer = tf.train.AdamOptimizer(.01).minimize(cost)
 
 	epochs = 1000
 
