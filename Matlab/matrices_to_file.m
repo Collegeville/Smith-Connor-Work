d8871af80f352c115ed7c8950dadcf9d535a5d37
connordsmith <connorsmith8729@gmail.com>
2017-08-01 09:54:31 -0500

    Add function that generates .csv data

--- Matlab/matrices_to_file.m
+++ Matlab/matrices_to_file.m
@@ -1,17 +1,39 @@
 function matrices_to_file(matrix_list)
 
-[m,n] = size(matrix_list);
-max = n;
+[m,max] = size(matrix_list);
+
+filename = ('matrix_data.csv');
+
+file = fopen(filename, 'w');
+
+fprintf(file,'%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', 'Size', 'Nonzeros', 'Symmetry', '2D/3D', 'Pos/Def', 'Kind', 'Solver', 'Tol', 'Maxit', 'Precond', 'droptol', 'diagcomp');
 
 for i=1:max
     Problem = UFget(matrix_list(i));
+    
     id = Problem.id;
+    
     A = Problem.A;
-    filename = sprintf('matrix%d', id)
-    file = fopen(filename, 'w');
-    [i,j,v] = find(A);
-    nzmax = size(v,1);
-    for ii=1:nzmax
-        fprintf(file,'%9d %9d %21.17e\n', i(ii), j(ii), v(ii));
-    end
+    
+    m = sprank(A);
+    
+    nonzeros = nnz(A);
+    
+    symmetry = issymmetric(A);
+    
+    kind = Problem.kind;
+    
+    solver = 'cg';
+    
+    tol = 1e-6;
+    
+    maxit = 100;
+    
+    precond = 'ichol';
+    
+    droptol = 1e-3;
+    
+    diagcomp = 0;
+
+    fprintf(file,'%9d,%9d,%9d, %9d, %9d, %s, %s, %9e, %9d, %s, %9e, %9d\n', m, nonzeros, symmetry, 1, 1, kind, solver, tol, maxit, precond, droptol, diagcomp);
 end
